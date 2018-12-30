3089908c64748b7e602cd7f11cb8353d97327d30
Martin Hoffmann <dev@marhoff.xyz>
2018-12-30 20:20:17 +0100

    Switching to pg_pmwget for handling of parallels urls queries

--- sql/function/items_json.sql
+++ sql/function/items_json.sql
@@ -1,14 +1,18 @@
 -- FUNCTION: hn_ranker.items_json(bigint[])
 
 CREATE OR REPLACE FUNCTION hn_ranker.items_json(
-  id_array bigint[],
-    wait numeric DEFAULT 0,
-    timeout numeric DEFAULT 5,
-    tries integer DEFAULT 1,
-    workers integer DEFAULT 100,
-    delimiter text DEFAULT '@wget_token@',
-  nbpasses integer DEFAULT 3)
-    RETURNS TABLE (id bigint, url url, payload jsonb, ts_end timestamptz, duration double precision, passes integer)
+  id_array bigint[]
+)
+RETURNS TABLE (
+  id bigint,
+  url url,
+  payload jsonb,
+  ts_end timestamptz,
+  duration double precision,
+  batch bigint,
+  retries integer,
+  batch_failrate double precision
+)
     LANGUAGE 'plpgsql'
     VOLATILE
 AS $BODY$
@@ -26,18 +30,33 @@ RAISE DEBUG 'wget_id : %', wget_id;
 
 
 RETURN QUERY
-WITH RECURSIVE
+WITH
 tunnest AS (SELECT DISTINCT tid FROM unnest(wget_id) tid ORDER BY tid),
 tsel AS (SELECT tid, format(wget_query,tid) url FROM tunnest),
 tget AS (
-  SELECT wget_urls.url, wget_urls.payload, wget_urls.ts_end, wget_urls.duration, 1 as passes
-    FROM wget_urls((SELECT array_agg(tsel.url)::url_array FROM tsel),
-      wait := wait , timeout := timeout, tries := tries, workers := workers, delimiter := delimiter)
-  UNION ALL
-  SELECT tget.url, wget_url(tget.url, wait := wait , timeout := timeout, tries := tries) payload, clock_timestamp()::timestamptz ts_end, NULL duration,tget.passes+1 as passes
-  FROM tget WHERE tget.payload IS NULL AND tget.passes < nbpasses
-  )
-SELECT tid id, tsel.url::url, tget.payload::jsonb, tget.ts_end, COALESCE(tget.duration,EXTRACT(EPOCH FROM (tget.ts_end-(lag(tget.ts_end) OVER (ORDER BY tget.ts_end asc))))), tget.passes
+  SELECT * FROM
+    wget_urls(
+      url_array := (SELECT array_agg(tsel.url)::url_array FROM tsel),
+      i_min_latency := 0,
+      i_timeout := 5,
+      i_tries := 1,
+      i_waitretry := 0,
+      i_parallel_jobs := 10,
+      i_delimiter := '@wget_token@'::text,
+      i_delay := 0,
+      r_min_latency := 0,
+      r_timeout := 5,
+      r_tries := 1,
+      r_waitretry := 0,
+      r_parallel_jobs := 10,
+      r_delimiter := '@wget_token@'::text,
+      r_delay := 0,
+      batch_size := 2000,
+      batch_retries := 1,
+      batch_retries_failrate := 0.05
+    )
+)
+SELECT tid id, tsel.url::url, tget.payload::jsonb, tget.ts_end, tget.duration, tget.batch, tget.retries, tget.batch_failrate
 FROM tsel LEFT JOIN tget ON tsel.url=tget.url
 AND tget.payload is NOT NULL;
 
