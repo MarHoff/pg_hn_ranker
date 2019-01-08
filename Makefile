2bb682d5f74125aa22cb6d1f753d497629df00f4
Martin Hoffmann <dev@marhoff.xyz>
2019-01-08 08:35:18 +0100

    Adding views to makefile & a diagnosis view for run_story

--- Makefile
+++ Makefile
@@ -10,6 +10,9 @@ FUNCTION := $(addprefix sql/function/, $(addsuffix .sql, $(FUNCTION)))
 TABLE := run story run_story
 TABLE := $(addprefix sql/table/, $(addsuffix .sql, $(TABLE)))
 
+VIEW := run_story_stats
+VIEW := $(addprefix sql/view/, $(addsuffix .sql, $(VIEW)))
+
 #TESTS = $(wildcard TEST/SQL/*.sql)
 
 usage:
@@ -17,9 +20,9 @@ usage:
 
 build : hn_ranker--dev.sql
 
-hn_ranker--dev.sql : $(DOMAIN) $(FUNCTION) $(TABLE)
+hn_ranker--dev.sql : $(DOMAIN) $(FUNCTION) $(TABLE) $(VIEW)
 	@echo 'Building develloper version'
-	cat $(DOMAIN) > $@ && cat $(FUNCTION) >> $@ && cat $(TABLE) >> $@
+	cat $(DOMAIN) > $@ && cat $(FUNCTION) >> $@ && cat $(TABLE) >> $@ && cat $(VIEW) >> $@
 
 #test:
 #	pg_prove -v --pset tuples_only=1 $(TESTS)
---
+++ sql/view/run_story_stats.sql
@@ -0,0 +1,20 @@
+-- View: @extschema@.run_story_stats
+
+CREATE VIEW @extschema@.run_story_stats AS
+SELECT run.id run_id,
+	count(*) FILTER (WHERE run_story.topstories_rank IS NOT NULL) topstories_count,
+	count(*) FILTER (WHERE run_story.beststories_rank IS NOT NULL) beststories_count,
+	count(*) FILTER (WHERE run_story.newstories_rank IS NOT NULL) newstories_count,
+	count(*) FILTER (WHERE run_story.status='new') new_count,
+	count(*) FILTER (WHERE run_story.status='hot') hot_count,
+	count(*) FILTER (WHERE run_story.status='tepid') tepid_count,
+	count(*) FILTER (WHERE run_story.status='cooling') cooling_count,
+	count(*) FILTER (WHERE run_story.status='cold') cold_count,
+	count(*) FILTER (WHERE run_story.status='frozen') frozen_count,
+	count(*) FILTER (WHERE run_story.payload IS NULL) fail_count,
+	count(*) total_count,
+	ts_run,
+	max(ts_payload)-min(ts_run) as fetch_duration
+FROM @extschema@.run LEFT JOIN @extschema@.run_story ON run.id=run_story.run_id
+	GROUP BY run.id
+	ORDER BY run.id desc;
