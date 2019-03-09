ea8b5aa0a0cc693d428b49212d27c72cabfa45b0
Martin Hoffmann <contact@marhoff.xyz>
2019-03-09 20:07:04 +0100

    Adding case for explicitly deleted story and storing failure as a status

--- sql/domain/story_status.sql
+++ sql/domain/story_status.sql
@@ -1,3 +1,3 @@
 --Type used in story & run_story
 
-CREATE TYPE hn_ranker.story_status AS ENUM ('new','hot','tepid','cooling','cold','frozen','missing');
+CREATE TYPE hn_ranker.story_status AS ENUM ('new','hot','tepid','cooling','cold','frozen','deleted','missing','failed');
