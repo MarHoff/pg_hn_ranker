9b3c4b1b07d4df348c50f358266dab6ed5cb8ab4
Martin Hoffmann <contact@marhoff.xyz>
2019-03-27 19:15:31 +0100

    Improve stats views and add diagnose_error as proper view from previous stub

--- sql/function/stub_show_run_error.sql
+++
@@ -1,5 +0,0 @@
-WITH run AS (SELECT max(id) FROM hn_ranker.run)
-SELECT run_id, object, object_id, a.*
-FROM hn_ranker.error, run
-LEFT JOIN LATERAL jsonb_to_record(error.report) AS a(id text, url url, payload text, ts_end timestamp with time zone, duration double precision, batch bigint, retries integer, batch_failrate double precision) ON true
-WHERE run_id=run.max
