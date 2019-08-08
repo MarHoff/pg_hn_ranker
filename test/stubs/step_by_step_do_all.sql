03a2fb3db4b05abcd1e60b1b9d5a58e60134f866
Martin Hoffmann <contact@marhoff.xyz>
2019-08-08 18:18:40 +0200

    Saving stub test for do_all

--- test/stubs/step_by_step_do_all.sql
+++ test/stubs/step_by_step_do_all.sql
@@ -1,10 +1,19 @@
---BEGIN;
---CALL hn_ranker.do_run('debug');
---SELECT * FROM hn_ranker.run WHERE id=currval('hn_ranker.run_id_seq'::regclass)::bigint
---SELECT * FROM hn_ranker.build_stories_ranks(ARRAY[currval('hn_ranker.run_id_seq'::regclass)::bigint])
---SELECT * FROM hn_ranker.build_stories_last(currval('hn_ranker.run_id_seq'::regclass));
-SELECT fetch_now, last_status, new_status, count(*) FROM hn_ranker.build_stories_classify(currval('hn_ranker.run_id_seq'::regclass),'debug')
-GROUP BY fetch_now, last_status, new_status
-ORDER BY fetch_now, last_status, new_status
-;
+BEGIN;
+CALL hn_ranker.do_all('production');
+/*
+SELECT * FROM hn_ranker.run WHERE id=currval('hn_ranker.run_id_seq'::regclass)::bigint
+
+SELECT * FROM hn_ranker.build_stories_ranks(ARRAY[currval('hn_ranker.run_id_seq'::regclass)::bigint])
+
+SELECT * FROM hn_ranker.build_stories_status(currval('hn_ranker.run_id_seq'::regclass)-1);
+
+SELECT fetch_now, (topstories_rank IS NULL AND beststories_rank IS NULL AND newstories_rank IS NULL) allranksnull,
+last_status, new_status, count(*)
+FROM hn_ranker.build_stories_classify(currval('hn_ranker.run_id_seq'::regclass),'production')
+GROUP BY fetch_now, (topstories_rank IS NULL AND beststories_rank IS NULL AND newstories_rank IS NULL), last_status, new_status
+ORDER BY fetch_now, (topstories_rank IS NULL AND beststories_rank IS NULL AND newstories_rank IS NULL), last_status, new_status;
+SELECT * FROM  hn_ranker.build_stories_classify(currval('hn_ranker.run_id_seq'::regclass),'production') LIMIT 10;
+
+*/
+
 --ROLLBACK;
